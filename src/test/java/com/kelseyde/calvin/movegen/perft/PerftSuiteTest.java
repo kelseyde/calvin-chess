1926d2e1c4eb6b4e01f4412dca3b165615cc0d3b
Astesana <jm-dev@astesana.net>
2025-01-21 16:16:50 +0100

    Perft:Fixes Stack overflow on negative depth + add Type arg

--- src/test/java/com/kelseyde/calvin/movegen/perft/PerftSuiteTest.java
+++ src/test/java/com/kelseyde/calvin/movegen/perft/PerftSuiteTest.java
@@ -7,13 +7,16 @@ import com.kelseyde.calvin.board.Board;
 import com.kelseyde.calvin.board.ChessVariant;
 import com.kelseyde.calvin.utils.Perft;
 import com.kelseyde.calvin.utils.Perft.Result;
+import com.kelseyde.calvin.utils.Perft.Type;
 import com.kelseyde.calvin.utils.notation.FEN;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
@@ -22,22 +25,21 @@ class PerftSuiteTest {
     @Test
     @DisabledIfSystemProperty(named="perftDepth", matches = "0")
     void testPerftSuite() throws IOException {
-    	doTestSuite("perftDepth", 2, Paths.get("src/test/resources/perft_suite.epd"), ChessVariant.STANDARD);
+    	doTestSuite("perftDepth", 2, Files.readAllLines(Paths.get("src/test/resources/perft_suite.epd")), ChessVariant.STANDARD);
     }
 
     @Test
     @DisabledIfSystemProperty(named="perftChess960Depth", matches = "0")
     void testChess960PerftSuite() throws IOException {
-    	doTestSuite("perftChess960Depth", 2, Paths.get("src/test/resources/perft_chess960_suite.epd"), ChessVariant.CHESS960);
+    	doTestSuite("perftChess960Depth", 2, Files.readAllLines(Paths.get("src/test/resources/perft_chess960_suite.epd")), ChessVariant.CHESS960);
     }
     
-    private void doTestSuite(String depthProperty, int defaultDepth, Path file, ChessVariant variant) throws IOException {
+    private void doTestSuite(String depthProperty, int defaultDepth, List<String> testLines, ChessVariant variant) {
 		final int depth = Integer.getInteger(depthProperty, defaultDepth);
-        List<String> lines = Files.readAllLines(file);
         if (depth!=defaultDepth) {
-        	System.out.println(depthProperty+": "+depth+", "+lines.size()+" lines");
+        	System.out.println(depthProperty+": "+depth+", "+testLines.size()+" lines");
         }
-        lines.stream().parallel().forEach(line -> {
+        testLines.stream().parallel().forEach(line -> {
             String[] parts = line.split(";");
             String fen = parts[0];
             if (parts.length<=depth) {
@@ -49,5 +51,21 @@ class PerftSuiteTest {
             final Result result = new Perft().perft(board, depth);
             assertEquals(expectedTotalMoves, result.leafNodesCount(), String.format("Fen: %s, Depth: %s, Expected: %s, Actual: %s", fen, depth, expectedTotalMoves, result.leafNodesCount()));
         });
+    }
+
+    @Test
+    void bulkTest() {
+    	final Board board = Board.from("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+    	Result perft = new Perft().perft(board, 3);
+    	final long count = perft.leafNodesCount();
+    	perft = new Perft().perft(board, 3, Type.BULK);
+    	assertEquals(count, perft.leafNodesCount());
+    }
+
+    @Test
+    void wrongDepthTest() {
+    	final Board board = Board.from("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+    	final Perft p = new Perft();
+    	assertThrows(IllegalArgumentException.class, () -> p.perft(board, -1));
     }
 }
