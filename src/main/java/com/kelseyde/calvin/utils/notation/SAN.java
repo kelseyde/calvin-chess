05227ceb0a84b14125186653fe9ab88cd8daec97
Astesana <jm-dev@astesana.net>
2025-03-19 10:21:52 +0100

    Starts fixing SAN bugs

--- src/main/java/com/kelseyde/calvin/utils/notation/SAN.java
+++ src/main/java/com/kelseyde/calvin/utils/notation/SAN.java
@@ -5,11 +5,22 @@ import com.kelseyde.calvin.movegen.MoveGenerator;
 
 import java.util.List;
 
+/**
+ * A move to <a href="https://en.wikipedia.org/wiki/Algebraic_notation_(chess)">Standard Algebraic Notation (SAN)</a> converter.
+ */
 public class SAN {
 
+    private SAN() {
+        super();
+    }
+
     /**
-     * Convert move to Standard Algebraic Notation (SAN)
-     * Note: the move must not yet have been made on the board
+     * Converts a move to its Standard Algebraic Notation (SAN)
+     * @param move the move to convert
+     * @param board the board on which the move is to be made.
+     * @return the move in SAN notation.
+     * <br>The SAN standard is relatively lax, some parts of the notation are optional (e.g. the <i>'e.p.'</i> when doing an en passant capture).
+     * <br>This method returns the variant used in the PGN standard (no <i>'e.p.'</i> for en passant captures).
      */
     public static String fromMove(Move move, Board board) {
         Piece piece = board.pieceAt(move.from());
@@ -21,9 +32,9 @@ public class SAN {
         }
 
         MoveGenerator moveGenerator = new MoveGenerator();
-        String notation = "";
+        final StringBuilder notation = new StringBuilder();
         if (piece != Piece.PAWN) {
-            notation += piece.code().toUpperCase();
+            notation.append(piece.code().toUpperCase());
         }
 
         // Check if any ambiguity exists in notation (e.g. if e2 can be reached via Nfe2 and Nbe2)
@@ -31,59 +42,50 @@ public class SAN {
             List<Move> legalMoves = moveGenerator.generateMoves(board);
 
             for (Move legalMove : legalMoves) {
+                if (legalMove.from() != move.from() && legalMove.to() == move.to() && board.pieceAt(legalMove.from()) == piece) {
+                    int fromFileIndex = File.of(move.from());
+                    int alternateFromFileIndex = File.of(legalMove.to());
+                    int fromRankIndex = Rank.of(move.from());
+                    int alternateFromRankIndex = Rank.of(legalMove.from());
 
-                if (legalMove.from() != move.from() && legalMove.to() == move.to()) {
-                    if (board.pieceAt(legalMove.from()) == piece) {
-                        int fromFileIndex = File.of(move.from());
-                        int alternateFromFileIndex = File.of(legalMove.to());
-                        int fromRankIndex = Rank.of(move.from());
-                        int alternateFromRankIndex = Rank.of(legalMove.from());
-
-                        if (fromFileIndex != alternateFromFileIndex) {
-                            notation += File.toNotation(move.from());
-                            break;
-                        }
-                        else if (fromRankIndex != alternateFromRankIndex)
-                        {
-                            notation += Rank.toRankNotation(move.from());
-                            break;
-                        }
+                    if (fromFileIndex != alternateFromFileIndex) {
+                        notation.append(File.toNotation(move.from()));
+                        break;
+                    }
+                    else if (fromRankIndex != alternateFromRankIndex)
+                    {
+                        notation.append(Rank.toRankNotation(move.from()));
+                        break;
                     }
                 }
             }
         }
 
-        if (captured != null) {
+        if (captured != null || move.isEnPassant()) {
             // add 'x' to indicate capture
             if (piece == Piece.PAWN) {
-                notation += File.toNotation(move.from());
+                notation.append(File.toNotation(move.from()));
             }
-            notation += "x";
-        }
-        else {
-            // Check if capturing en passant
-            if (move.isEnPassant()) {
-                notation += File.toNotation(move.from()) + "x";
-            }
+            notation.append("x");
         }
 
-        notation += File.toNotation(move.to());
-        notation += Rank.toRankNotation(move.to());
+        notation.append(File.toNotation(move.to()));
+        notation.append(Rank.toRankNotation(move.to()));
 
         // Add promotion piece type
         if (move.promoPiece() != null) {
             Piece promotionPieceType = move.promoPiece();
-            notation += "=" + promotionPieceType.code().toUpperCase();
+            notation.append("=" + promotionPieceType.code().toUpperCase());
         }
 
         board.makeMove(move);
         if (moveGenerator.isCheck(board, board.isWhite())) {
             List<Move> legalMoves = moveGenerator.generateMoves(board);
-            notation += legalMoves.isEmpty() ? "#" : "+";
+            notation.append(legalMoves.isEmpty() ? "#" : "+");
         }
         board.unmakeMove();
 
-        return notation;
+        return notation.toString();
     }
 
 }
